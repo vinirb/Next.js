f38b1a78670ca00a9917804be096480f552de632
Vinicius Rolim Barbosa <vinicius.b@ufms.br>
2022-10-24 22:19:46 -0400

    iniciando projeto em Next.js

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function PaginaDePerguntas(){
+    return(
+        <div>
+            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <Link href="/">
+            Ir para o home
+            </Link>
+        </div>
+    )
+}
---
+++ src/components/Link/index.js
@@ -0,0 +1,10 @@
+import NextLink from 'next/link';
+import LinkEstilizado from '../LinkEstilizado';
+
+export default function Link({ children, href, ...props }) {
+  return (
+    <NextLink href={href}>
+      <LinkEstilizado {...props}>{children}</LinkEstilizado>
+    </NextLink>
+  );
+}
